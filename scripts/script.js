d4e5c9c4be2f5f36e6011640d53b039c7ace9103
Elizaveta <josephine.rus@gmail.com>
2026-03-14 23:27:49 +0300

    add modal

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,25 @@
 // Smooth
 
 window.addEventListener('scroll', function () { 
-   const header = document.querySelector('.header'); 
-   if (window.scrollY > 100) { 
-      header.classList.add('scroll'); 
-   } 
-   else { 
-      header.classList.remove('scroll'); 
-   } 
+    const header = document.querySelector('.header'); 
+    if (window.scrollY > 100) { 
+        header.classList.add('scroll'); 
+    } 
+    else { 
+        header.classList.remove('scroll');
+    } 
 });
+
+// Burger Menu
+
+const btnBurger = document.getElementById("btnBurger");
+const modal = document.getElementById("modal");
+const btnClose = document.getElementById("btnClose");
+
+btnBurger.addEventListener("click", function () {
+    modal.classList.add("active");
+})
+
+btnClose.addEventListener("click", function () {
+    modal.classList.remove("active");
+})
